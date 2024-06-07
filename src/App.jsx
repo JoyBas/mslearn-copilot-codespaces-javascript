ef947390113fe9d8e9c112cebcdd9f36d649f1b2
JoyBas <106107987+JoyBas@users.noreply.github.com>
2024-06-07 12:14:08 +0000

    chore: Update contact information and social media links

--- src/App.jsx
+++ src/App.jsx
@@ -24,15 +24,15 @@ import "./styles.css";
  * If you don't have one of the social sites listed, leave it as an empty string.
  */
 const siteProps = {
-  name: "Alexandrie Grenier",
-  title: "Web Designer & Content Creator",
-  email: "alex@example.com",
-  gitHub: "microsoft",
-  instagram: "microsoft",
-  linkedIn: "satyanadella",
+  name: "Joy Bassey",
+  title: "Tester",
+  email: "joy.ubassey@gmail.com",
+  gitHub: "https://github.com/JoyBas",
+  instagram: "",
+  linkedIn: "https://www.linkedin.com/in/joy-bassey-ab0477a2/",
   medium: "",
-  twitter: "microsoft",
-  youTube: "Code",
+  twitter: "",
+  youTube: "",
 };
 
 const primaryColor = "#4E567E";
